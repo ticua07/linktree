a49999c174400604d7d2d4a100bea9472e371b62
Ticua <ticua@ticua.ar>
2024-01-14 19:46:08 -0300

    styled login page, functinality missing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,40 +6,9 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
 
 export default function Login() {
-    const [user, setUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [isLogin, setIsLogin] = useState(true)
     const supabase = createClientComponentClient();
 
-    const logIn = async () => {
-        await supabase.auth.signInWithPassword({
-            email,
-            password
-        })
-    }
-    const signUp = async () => {
-        if (user.trim() === '' || email.trim() === '' || password.trim() === '') {
-            return;
-        }
-
-        let userData = await supabase.auth.signUp({
-            email,
-            password
-        })
-
-        await supabase.from("usernames").insert({
-            userid: userData.data.user?.id,
-            username: user
-        })
-
-        await supabase.from("links").insert({
-            userid: userData.data.user?.id,
-            links: JSON.stringify({})
-        })
-    }
-    const signOut = async () => {
-        await supabase.auth.signOut()
-    }
 
     useEffect(() => {
         const getData = async () => {
@@ -51,17 +20,33 @@ export default function Login() {
     }, [])
 
     return (
-        <div className={styles.container}>
+        <>
+            {isLogin
+                ? <div className={styles.container}>
+                    <div className={styles.title_container}>
+                        <h1 className={styles.title}>Log in</h1>
+                        <div className={styles.form_container}>
+                            <input className={[styles.form_elements, styles.input].join(" ")} type="email" placeholder="Email" />
+                            <input className={[styles.form_elements, styles.input].join(" ")} type="password" placeholder="Password" />
+                            <button className={[styles.form_elements, styles.button].join(" ")}>Log in</button>
+                            <span onClick={() => setIsLogin(!isLogin)} className={[styles.form_elements, styles.change_method].join(" ")}>Or, create an account</span>
+                        </div>
+                    </div>
+                </div>
 
-            <div className={styles.container_buttons}>
-                <button onClick={logIn} className={styles.button}>log in</button>
-                <button onClick={signUp} className={styles.button}>sign up</button>
-                <button onClick={signOut} className={styles.button}>sign out</button>
-            </div>
-
-            <input value={user} type="text" onChange={(e) => setUser(e.target.value)} placeholder='username'></input>
-            <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder='email'></input>
-            <input value={password} type="text" onChange={(e) => setPassword(e.target.value)} placeholder='password'></input>
-        </div>
+                : <div className={styles.container}>
+                    <div className={styles.title_container}>
+                        <h1 className={styles.title}>Log in</h1>
+                        <div className={styles.form_container}>
+                            <input className={[styles.form_elements, styles.input].join(" ")} type="text" placeholder="Username" />
+                            <input className={[styles.form_elements, styles.input].join(" ")} type="email" placeholder="Email" />
+                            <input className={[styles.form_elements, styles.input].join(" ")} type="password" placeholder="Password" />
+                            <button className={[styles.form_elements, styles.button].join(" ")}>Log in</button>
+                            <span onClick={() => setIsLogin(!isLogin)} className={[styles.form_elements, styles.change_method].join(" ")}>Or, login to an account</span>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
