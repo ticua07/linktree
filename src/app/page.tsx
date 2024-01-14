9e6499e0f2bb92fc40d5e4b290afeaf0a9f76c25
Ticua <ticua@ticua.ar>
2024-01-14 19:44:59 -0300

    Styled main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 'use client';
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { User, createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useUser } from "../../hooks/hooks";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css"
 
 export default function Home() {
     const supabase = createClientComponentClient();
-    const user = useUser();
+    const [user, setUser] = useState<User | null>();
+
+    useEffect(() => {
+        const getUser = async () => {
+            setUser(await useUser())
+        }
+        getUser()
+    }, []);
+
 
     return (
-        <div>
-            {user ? user?.email : <p>Loading....</p>}
+        <div className={styles.container}>
+            <div className={styles.title_container}>
+                <h1 className={styles.title}>Welcome to Linker</h1>
+                <h2 className={styles.subtitle}>Your one-stop solution for managing all your online presence.<br />Simplify your profile with Linker. </h2>
+                <div className={styles.button_container}>
+                    <a href="/login" className={styles.get_started}>Get started</a>
+                </div>
+            </div>
         </div>
     )
 }
